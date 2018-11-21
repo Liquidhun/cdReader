b0e673bdd4ec418a05c36899d25b59efb9a34ee6
Zoltan Lengyel <zoltanlengyel77@gmail.com>
2018-11-21 13:52:59 +0100

    init

---
+++ readCd/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace readCd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            string dir = @"R:\"; //CD meghajtó betűjele
+            string newDriver = @"D:\TESZT"; //a célmeghajtó betűjele
+            bool firstRun = true;
+            string cdNumber;
+            cdNumber = "";
+            ConfigurationSettings.GetConfig("CD_ROM");
+            //Saving file as backup
+            //string sourceFile = Path.Combine("", "test.txt");
+            //string destFile = System.IO.Path.Combine(".", "test.txt");
+            //string randomName = DateTime.Today;
+            Console.WriteLine();
+            Console.ReadKey();
+            while (true)
+            {
+                Console.Clear();
+                if (firstRun || cdNumber=="")
+                {
+                    Console.WriteLine("Add meg a CD sorszámát, vagy nyomj q betűt a kilépéshez:");
+                    firstRun = false;
+                }
+                else
+                {
+                    Console.WriteLine("Sikeresen feldolgozva. Add meg a következő CD sorszámát, vagy nyomj q betűt a kilépéshez:");
+                }
+                cdNumber = Console.ReadLine();
+                if (cdNumber.ToLower() == "q")
+                {
+                    break;
+                }
+                if (cdNumber != "")
+                {
+                    Reader reader = new Reader(dir, newDriver, cdNumber);
+                    StringBuilder sb = new StringBuilder();
+                    timer.Start();
+                    reader.readAll(dir, sb, newDriver);
+                    timer.Stop();
+                    using (StreamWriter sw = new StreamWriter(new FileStream(@"list.txt", FileMode.Append)))
+                    {
+                        sw.Write(sb.ToString());
+                    }
+                    Console.WriteLine(timer.Elapsed.TotalSeconds + " másodperc alatt futott le.");
+                    //Console.ReadKey();
+                }
+                //else Console.Clear();
+
+            }
+        }
+    }
+}
---
+++ readCd/Reader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readCd
+{
+    class Reader
+    {
+        public string Dir;
+        public string NewDrive;
+        public string CdNumber;
+        public int step = 0;
+        public string newFile;
+        public Reader(string dir, string newDrive, string cdNumber)
+        {
+            Dir = dir;
+            NewDrive = newDrive;
+            CdNumber = cdNumber;
+        }
+        public void getFilesFrom(string fdir, StringBuilder sb, string newFile)
+        {
+            foreach (string f in Directory.GetFiles(fdir))
+            {
+                FileInfo info = new FileInfo(f);
+                sb.Append(CdNumber);
+                sb.Append(";");
+                sb.Append(info.Name);
+                sb.Append(";");
+                sb.Append(info.CreationTime);
+                sb.Append(";");
+                sb.Append(info.LastWriteTime);
+                sb.Append(";");
+                sb.Append(info.Length);
+                sb.AppendLine(";");
+                //info.CopyTo(info.FullName.Replace(Dir, Path.Combine(NewDrive, CdNumber)));
+                info.CopyTo(Path.Combine(newFile,info.Name));
+                Progress();
+                Console.WriteLine(" > " + info.FullName.Replace(Dir, Path.Combine(NewDrive, CdNumber)));
+            }
+        }
+
+        public void Progress()
+        {
+            Console.SetCursorPosition(0, 2);
+            switch (step)
+            {
+                case 0:
+                    Console.Write("|");
+                    break;
+                case 1:
+                    Console.Write("/");
+                    break;
+                case 2:
+                    Console.Write("-");
+                    break;
+                case 3:
+                    Console.Write(@"\");
+                    break;
+            }
+            step++;
+            if (step == 4) { step = 0; }
+        }
+        public void readAll(string dir, StringBuilder sb, string newDriver)
+        {
+            //Console.WriteLine("Feldolgozás alatt álló mappa: " + dir);
+            Progress();
+            //Console.WriteLine(dir.Replace(Dir, Path.Combine(NewDrive, CdNumber)));
+            Directory.CreateDirectory(dir.Replace(Dir, Path.Combine(NewDrive, CdNumber)));
+            newFile = dir.Replace(Dir, Path.Combine(NewDrive, CdNumber));
+            getFilesFrom(dir, sb, newFile);
+            foreach (string kdir in Directory.GetDirectories(dir))
+            {
+                readAll(kdir, sb, newDriver);
+            }
+        }
+    }
+}
